923413804ca13ce88366cd9f76d39f916f3745cb
Bùi Đình Quân <149028296+buidinhquan13@users.noreply.github.com>
2024-02-21 14:49:49 +0700

    Add files via upload

---
+++ retrieve_fn.py
@@ -0,0 +1,72 @@
+import torch
+
+import faiss
+from torchvision import transforms
+from model import FeatureExtractor
+import bisect
+from dataset import RParisDataset
+from pathlib import Path
+from setup import workdir
+
+root = Path(workdir)
+
+transform = transforms.Compose(
+    [
+        transforms.Resize(
+            (224, 224), interpolation=transforms.InterpolationMode.BILINEAR
+        ),
+        transforms.ToTensor(),
+    ]
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+feature_root = root / "roxford-rparis" / "embedding"
+
+resnet_l2_index = faiss.read_index(str(feature_root / "rparis_resnet50.l2_index.bin"))
+
+aug_resnet_l2_index = faiss.read_index(
+    str(feature_root / "cropped_rparis_resnet50.l2_index.bin")
+)
+
+hog_l2_index = faiss.read_index(str(feature_root / "rparis_hog.l2_index.bin"))
+
+extractor = FeatureExtractor(device)
+
+ds = RParisDataset(transform=None)
+
+
+def retrieve(query_img, k, index=resnet_l2_index):
+    img = transform(query_img)
+    img = img.unsqueeze(0).to(device)
+
+    feat = extractor.extract_features(img)
+    distance, indices = index.search(feat, k)
+
+    return indices[0].tolist(), distance[0].tolist()
+
+
+def retrieve_augmented(query_img, k):
+    src_indices, src_dst = retrieve(query_img, k, index=resnet_l2_index)
+    src_indices, src_dst = extend_answer_on_augment_ds(
+        query_img, k * 4, src_indices, src_dst, aug_index=aug_resnet_l2_index
+    )
+    return src_indices, src_dst
+
+
+def extend_answer_on_augment_ds(query_im, k, src_indices, src_dst, aug_index):
+    aug_indices, aug_dst = retrieve(query_im, k, index=aug_index)
+    for idx, aug_ans in enumerate(aug_indices):
+        original_idx = aug_ans // 4
+
+        if original_idx not in src_indices:
+            ins_pos = bisect.bisect_left(src_dst, aug_dst[idx])
+            src_indices.insert(ins_pos, original_idx)
+            src_dst.insert(ins_pos, aug_dst[idx])
+
+    return src_indices, src_dst
+
+
+def get_image_from_index(idx):
+    return ds[idx]
---
+++ streamlit.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from streamlit_cropper import st_cropper
+from PIL import Image, ImageFilter
+
+from retrieve_fn import *
+import time
+from itertools import cycle
+
+
+st.set_page_config(layout="wide")
+
+
+st.title("CONTENT-BASED IMAGE RETRIEVAL")
+col1, col2 = st.columns(2)
+with col1:
+    st.header("QUERY")
+
+    st.subheader("Upload image")
+    img_file = st.file_uploader(label="", type=["png", "jpg"])
+
+    st.subheader("Select feature space")
+    feature_space_opt = st.selectbox(
+        "",
+        (
+            "ResNet50",
+            "ResNet50 (Augmented)",
+            # "HOG",
+        ),
+    )
+
+    if img_file:
+        img = Image.open(img_file)
+        st.subheader("Result size")
+        k = st.slider("", 1, 100, 10)
+        st.subheader("Option")
+        img_processing_opt = st.selectbox(
+            "",
+            (
+                "Original",
+                "Crop",
+                "Blur",
+                "Sharpen",
+                "Smooth",
+                "Edge",
+                "Horizontal Flip",
+                "Vertical Flip",
+            ),
+        )
+        st.write("Preview")
+
+        match img_processing_opt:
+            case "Original":
+                pass
+            case "Blur":
+                img = img.filter(ImageFilter.BLUR)
+            case "Sharpen":
+                img = img.filter(ImageFilter.SHARPEN)
+            case "Smooth":
+                img = img.filter(ImageFilter.SMOOTH)
+            case "Edge":
+                img = img.filter(ImageFilter.FIND_EDGES)
+            case "Horizontal Flip":
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            case "Vertical Flip":
+                img = img.transpose(Image.FLIP_TOP_BOTTOM)
+            case "Crop":
+                # Get a cropped image from the frontend
+                img = st_cropper(img, realtime_update=True, box_color="#FF0004")
+                st.write("Preview (Resized)")
+                _ = img.thumbnail((250, 250))
+                st.image(img)
+            case _:
+                pass
+        if img_processing_opt != "Crop":
+            st.image(img)
+
+
+with col2:
+    st.header("RESULT")
+    if img_file:
+        start = time.time()
+        if feature_space_opt == "ResNet50":
+            indices, scores = retrieve(img, k)
+        elif feature_space_opt == "ResNet50 (Augmented)":
+            indices, scores = retrieve_augmented(img, k)
+        # elif feature_space_opt == "HOG":
+        #     indices, scores = retrieve(img, k, index=hog_l2_index)
+        end = time.time()
+
+        st.markdown(f"**Finish in {(end - start):.2f} seconds**")
+        caption = []
+        filteredImages = []
+        indices = indices[:k]
+        scores = scores[:k]
+        for idx, img_idx in enumerate(indices):
+            img, name = get_image_from_index(img_idx)
+            filteredImages.append(img)
+            caption.append(f"{name.split('_')[1]}({scores[idx]:.2f})")
+
+        cols = cycle(
+            st.columns(3)
+        )  # st.columns here since it is out of beta at the time I'm writing this
+        for idx, filteredImage in enumerate(filteredImages):
+            next(cols).image(filteredImage, width=150, caption=caption[idx])
